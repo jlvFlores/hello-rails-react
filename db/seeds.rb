bc8b493421e2b1fbbe12776017b3850821a6616d
jlvFlores <jluisvazquez.jv@gmail.com>
2023-08-21 20:49:35 -0600

    Add messages API endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(content: "Hello, Rails!")
+Message.create(content: "Hello, React!")
+Message.create(content: "Rosa Pastel")
+Message.create(content: "Hola Guapo")
+Message.create(content: "Pretty bird fui fui")
